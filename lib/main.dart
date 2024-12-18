c8a3c437b20eed47a7be1d696e72bc51d87e8cb8
Carlos DIaz <c.diazadriann@gmail.com>
2024-12-18 11:30:10 -0400

    Launcher Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:external_app_launcher/external_app_launcher.dart';
+import 'package:android_intent_plus/android_intent.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,7 +30,6 @@ class MyAppState extends State<MyApp> {
 
   @override
   void dispose() {
-    // Cancelar el Timer cuando el widget se destruye
     _timer?.cancel();
     super.dispose();
   }
@@ -46,6 +46,13 @@ class MyAppState extends State<MyApp> {
     }
   }
 
+  Future<void> openSettings() async {
+    const intent = AndroidIntent(
+      action: 'android.settings.SETTINGS',
+    );
+    await intent.launch();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -53,16 +60,48 @@ class MyAppState extends State<MyApp> {
       theme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Launcher Lottery'),
+          title: const Text('Launcher'),
         ),
-        body: Center(
-            child: FilledButton.icon(
-          onPressed: () {
-            init();
-          },
-          label: const Text("Open"),
-          icon: const Icon(Icons.open_in_new),
-        )),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20), // Espaciado arriba
+            ClipOval(
+              child: Container(
+                width: 250,
+                height: 250,
+                child: Image.asset(
+                  'assets/icon.png', // Ruta de tu imagen en los assets
+                  fit: BoxFit.cover, // Ajusta la imagen dentro del círculo
+                ),
+              ),
+            ),
+            const SizedBox(
+                height: 30), // Espaciado entre la imagen y los botones
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FilledButton.icon(
+                    onPressed: () {
+                      init();
+                    },
+                    label: const Text("Open App"),
+                    icon: const Icon(Icons.open_in_new),
+                  ),
+                  const SizedBox(height: 20),
+                  FilledButton.icon(
+                    onPressed: () {
+                      openSettings();
+                    },
+                    label: const Text("Open Settings"),
+                    icon: const Icon(Icons.settings),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
